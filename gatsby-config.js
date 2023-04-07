62e2353803831ddc60883b1d6de26e723e99cd47
Xavi Gonzalvo <xavigonzalvo@google.com>
2023-04-07 07:45:37 -0400

    No gtag.

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,19 +4,17 @@
 module.exports = {
   siteMetadata: {
     title: `Xavi's page`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-google-gtag", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-mdx", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+  plugins: [
+    "gatsby-plugin-mdx",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
     },
-    __key: "pages"
-  }]
+  ],
 };
